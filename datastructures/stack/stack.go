55daa40ab60d54ea37c29c67e8944ee4868d5a50
Raju Ahmed Shetu <shetu2153@gmail.com>
2022-05-12 09:58:06 -0400

    updated the stack ds

--- datastructures/stack/stack.go
+++ datastructures/stack/stack.go
@@ -1,1 +1,40 @@
 package stack
+
+import linkedlist "github.com/nashmaniac/golang-coding/datastructures/linked_list"
+
+type Stack interface {
+	Push(value linkedlist.Element)
+	Pop() linkedlist.Element
+	Peek() linkedlist.Element
+	IsEmpty() bool
+}
+
+type stack struct {
+	List linkedlist.LinkedList
+}
+
+// IsEmpty implements Stack
+func (s *stack) IsEmpty() bool {
+	return s.List.Empty()
+}
+
+// Peek implements Stack
+func (s *stack) Peek() linkedlist.Element {
+	return s.List.Front()
+}
+
+// Pop implements Stack
+func (s *stack) Pop() linkedlist.Element {
+	return s.List.PopFront()
+}
+
+// Push implements Stack
+func (s *stack) Push(value linkedlist.Element) {
+	s.List.PushFront(value)
+}
+
+func NewStack() Stack {
+	return &stack{
+		List: linkedlist.NewSinglyLinkedList[int](),
+	}
+}
